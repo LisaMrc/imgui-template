969cb54d572d8c66bf73fe0a9290371305155654
Jean LE CHAUDELEC <132900223+Jeanlc-citations@users.noreply.github.com>
2025-03-17 09:36:51 +0100

    FreeflyCamera [DONE]

---
+++ include/FreeflyCamera.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+enum class CameraMode {
+    Trackball,
+    PieceView
+};
+
+
+class FreeflyCamera {
+    private:
+        glm::vec3 m_Position;
+        float m_fPhi;
+        float m_fTheta;
+    
+        glm::vec3 m_FrontVector;
+        glm::vec3 m_LeftVector;
+        glm::vec3 m_UpVector;
+
+        glm::vec3 m_PiecePosition;
+
+        void computeDirectionVectors();
+    
+    public:
+        FreeflyCamera();
+        void attachToPiece(glm::vec3 piecePosition);
+        // void moveLeft(float t);
+        // void moveFront(float t);
+        void rotateLeft(float degrees);
+        void rotateUp(float degrees);
+        glm::mat4 getViewMatrix() const;
+        
+    };
